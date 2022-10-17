2d6d0ac0ee5c7dca1903b04d93f7cb13444d34ec
Carlos <carlosveryan4@gmail.com>
2022-10-16 19:01:29 -0500

    Finished Heap Tree

--- Priority Queue/PQ.cpp
+++ Priority Queue/PQ.cpp
@@ -4,12 +4,14 @@
 
 using namespace std;
 
+// Class for Heap Tree
 struct Node
 {
     int value;
     int priority;
 };
 
+// Class for Priority Queue
 class priority_queue
 {
 private:
@@ -27,70 +29,69 @@ public:
     {
         delete[] PQ;
     }
-    void push(int);
-    Node *pop();
-    int top();
-    bool empty();
-    int size();
+    void push(int); // Insert a node in the priority queue O(logN)
+    Node *pop();    // Delete a node from the priority queue O(logN)
+    int top();      // Return the value of the node with highest priority O(1)
+    bool empty();   // Check if the priority queue is empty O(1)
+    int size();     // Return the size of the priority queue O(1)
 
-    void displayIndex(int);
-    void display();
+    void displayIndex(int); // Display the value and priority of a node at a given index O(1)
+    void display();         // Display the value and priority of all nodes in the priority queue O(N)
 };
 
-void priority_queue::push(int value)
+void priority_queue::push(int value) // Insert a node in the priority queue O(logN)
 {
     if (NumNodes == N - 1)
     {
         cout << "Priority Queue is full" << endl;
-        return; // Salir del ciclo
+        return; // exit the function
     }
     // Increase the size
     NumNodes++;
 
     // Insert the element
-
     PQ[NumNodes].value = value;
 }
 
-Node *priority_queue::pop()
+Node *priority_queue::pop() // Delete a node from the priority queue O(logN)
 {
-    if (NumNodes == -1)
+    if (NumNodes == -1) // Check if the priority queue is empty
     {
-        cout << "Priority Queue is Empty" << endl;
-        return NULL;
+        cout << "Priority Queue is empty" << endl;
+        return NULL; // exit the function
     }
 
-    int ind = top();
+    int ind = top(); // Get the index of the node with highest priority
 
-    Node *Aux = &PQ[ind];
-    cout << "Element " << Aux->value << " removed" << endl;
+    Node *Aux = &PQ[ind];                                   // Create a pointer to the node with highest priority
+    cout << "Element " << Aux->value << " removed" << endl; // Display the value of the node with highest priority
 
-    for (int i = ind; i < NumNodes; i++)
+    for (int i = ind; i < NumNodes; i++) // Shift all the nodes to the left
     {
         PQ[i] = PQ[i + 1];
     }
 
-    NumNodes--;
+    NumNodes--; // Decrease the size
 
-    return Aux;
+    return Aux; // Return the pointer to the node with highest priority
 }
 
-int priority_queue::top()
-{ // Finds the position of the element with highest priority
-    int highestPriority = INT_MIN;
-    int ind = -1;
+int priority_queue::top() // Finds the position of the element with highest priority
+{
+    int highestPriority = INT_MIN; // Initialize the highest priority with the minimum value of an integer
+    int ind = -1;                  // Initialize the index with -1
 
-    for (int i = 0; i <= NumNodes; i++)
+    for (int i = 0; i <= NumNodes; i++) // Iterate trough all the nodes
     {
-        if (highestPriority < PQ[i].priority)
+        if (highestPriority < PQ[i].priority) // Check if the priority of the current node is higher than the highest priority
         {
-            highestPriority = PQ[i].priority;
-            ind = i;
+            highestPriority = PQ[i].priority; // Update the highest priority
+            ind = i;                          // Update the index
         }
-        else if (highestPriority == PQ[i].priority && PQ[i].value > PQ[ind].value)
+        else if (highestPriority == PQ[i].priority && PQ[i].value > PQ[ind].value) // Check if the priority of the current node is equal to the highest priority and the value of the current node is higher than the value of the node with highest priority
         {
-            highestPriority = PQ[i].priority;
-            ind = i;
+            highestPriority = PQ[i].priority; // Update the highest priority
+            ind = i;                          // Update the index
         }
 
         return ind;
@@ -98,12 +99,12 @@ int priority_queue::top()
     return ind;
 }
 
-void priority_queue::displayIndex(int idx)
+void priority_queue::displayIndex(int idx) // Display the value and priority of a node at a given index
 {
     cout << PQ[idx].value << endl;
 }
 
-void priority_queue::display()
+void priority_queue::display() // Display the value and priority of all nodes in the priority queue
 {
     cout << endl
          << "Elements: ";
@@ -116,7 +117,7 @@ void priority_queue::display()
          << endl;
 }
 
-bool priority_queue::empty()
+bool priority_queue::empty() // Check if de PQ is empty
 {
     if (NumNodes == -1)
     {
@@ -128,17 +129,19 @@ bool priority_queue::empty()
     }
 }
 
-int priority_queue::size()
+int priority_queue::size() // Return the size of the PQ
 {
     return NumNodes + 1;
 }
 
 int main()
 {
+    // Create a priority queue
     vector<int> values = {45, 25, 33, 45, 51, 64, 75, 81, 93, 18};
     int size = values.size();
     priority_queue P(size);
 
+    // Insert the elements
     for (int i = 0; i < size; i++)
     {
         P.push(values[i]);
@@ -148,6 +151,7 @@ int main()
          << "Size: " << P.size();
     P.display();
 
+    // Delete the elements
     while (not P.empty())
     {
         int index = P.top();
